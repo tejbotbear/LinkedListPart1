be0a235dbb2e0048ab2c81ea0d6440595ebf1795
tejbotbear <89226541+tejbotbear@users.noreply.github.com>
2023-01-11 14:06:24 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
-#include "student.h"
+#include <iostream>
+#include "Node.h"
 
 using namespace std;
 
-int main() {
+void add (int newvalue);
+void print(Node* next);
 
+Node* head = NULL;
+
+int main() {
+ add(5);
+ print(head);
+ add(7);
+ print(head);
+ add(2);
+ print(head);
 }
+
+void add(int newvalue) {
+  Node* current = head;
+  if (current == NULL) {
+  head = new Node();
+  head->setvalue(newvalue);
+  }
+  else {
+    while (current->getNext() != NULL) {
+    current = current->getNext();
+   }
+    current->setNext(new Node());
+    current->getNext()->setValue(newvalue);
+}
+  
+void print(Node* next) {
+  if (next == head) {
+    cout << "List:";
+  }
+  if (next != NULL) {
+    cout << next->getValue() << " ";
